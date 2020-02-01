98c8a3762cac1ddd79dffc83391080a71ce3f93c
Alexander Medina <alexjmedina@users.noreply.github.com>
2020-02-01 14:42:58 -0500

    Create DetermineProblemType.py

---
+++ Preparation/DetermineProblemType.py
@@ -0,0 +1,6 @@
+# The train DataFrame is already available in your workspace. It has the target variable column called "sales"
+import matplotlib.pyplot as plt
+
+# PLot a Histogram
+train.hist(bins=30, alpha=0.5)
+plt.show()
