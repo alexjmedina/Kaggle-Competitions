161719de966733993c2a443e655196b89a51c5cd
Alexander Medina <alexjmedina@users.noreply.github.com>
2020-02-01 14:32:04 -0500

    Update ExploreTestData.py

--- Preparation/ExploreTestData.py
+++ Preparation/ExploreTestData.py
@@ -6,3 +6,9 @@ test = pd.read_csv('test.csv')
 # Print train and test columns
 print('Train columns:', train.columns.tolist())
 print('Test columns:', test.columns.tolist())
+
+# Read the sample submission file
+sample_submission = pd.read_csv('sample_submission.csv')
+
+# Look at the head() of the sample submission
+print(sample_submission.head())
