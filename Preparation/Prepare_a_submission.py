be2ff399e8de6c1bc6891a58c216f5a3e4ff949a
Alexander Medina <alexjmedina@users.noreply.github.com>
2020-02-01 16:24:42 -0500

    Create Prepare_a_submission.py

---
+++ Preparation/Prepare_a_submission.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Read test and sample submission data
+test = pd.read_csv('test.csv')
+sample_submission = pd.read_csv('sample_submission.csv')
+
+# Show the head() of the sample_submission
+print(sample_submission.head())
