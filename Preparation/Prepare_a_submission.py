24b8aebd40a8c76fb39bb9374009969f9e153f01
Alexander Medina <alexjmedina@users.noreply.github.com>
2020-02-01 16:40:24 -0500

    Update Prepare_a_submission.py

--- Preparation/Prepare_a_submission.py
+++ Preparation/Prepare_a_submission.py
@@ -6,3 +6,9 @@ sample_submission = pd.read_csv('sample_submission.csv')
 
 # Show the head() of the sample_submission
 print(sample_submission.head())
+
+# Get predictions for the test set
+test['sales'] = rf.predict(test[['store', 'item']])
+
+# Write test predictions using the sample_submission format
+test[['id', 'sales']].to_csv('kaggle_submission.csv', index=False)
