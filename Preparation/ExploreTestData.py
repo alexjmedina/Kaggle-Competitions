e45d9873005bfcf1bd982fc37d28da3cc14d317c
Alexander Medina <alexjmedina@users.noreply.github.com>
2020-02-01 14:29:42 -0500

    Create ExploreTestData.py

---
+++ Preparation/ExploreTestData.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Read the test data
+test = pd.read_csv('test.csv')
+
+# Print train and test columns
+print('Train columns:', train.columns.tolist())
+print('Test columns:', test.columns.tolist())
