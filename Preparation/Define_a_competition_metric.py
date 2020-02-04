a050f0117c288a69634d533873d91b30d386cf7a
Alexander Medina <alexjmedina@users.noreply.github.com>
2020-02-04 17:14:03 -0500

    Create Define_a_competition_metric.py

---
+++ Preparation/Define_a_competition_metric.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Import MSE from sklearn
+from sklearn.metrics import mean_squared_error
+
+# Define your own MSE function
+def own_mse(y_true, y_pred):
+  	# Raise differences to the power of 2
+    squares = np.power(y_true - y_pred, 2)
+    # Find mean over all observations
+    err = np.sqrt(np.mean(squares))
+    return err
+
+print('Sklearn MSE: {:.5f}. '.format(mean_squared_error(y_regression_true, y_regression_pred)))
+print('Your MSE: {:.5f}. '.format(own_mse(y_regression_true, y_regression_pred)))
