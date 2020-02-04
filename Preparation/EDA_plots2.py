143ba8934f30cd30847c9e8a8c99778ac0f3c0d0
Alexander Medina <alexjmedina@users.noreply.github.com>
2020-02-04 18:37:21 -0500

    Create EDA_plots2.py

---
+++ Preparation/EDA_plots2.py
@@ -0,0 +1,15 @@
+# Plot the median fare amount for each hour of the day as a simple line plot.
+# Create hour feature
+train['pickup_datetime'] = pd.to_datetime(train.pickup_datetime)
+train['hour'] = train.pickup_datetime.dt.hour
+
+# Find median fare_amount for each hour
+hour_price = train.groupby('hour', as_index=False)['fare_amount'].median()
+
+# Plot the line plot
+plt.plot(hour_price['hour'], hour_price['fare_amount'], marker='o')
+plt.xlabel('Hour of the day')
+plt.ylabel('Median fare amount')
+plt.title('Fare amount based on day time')
+plt.xticks(range(24))
+plt.show()
