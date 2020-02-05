70ed07e68bcdf553bd75b939d67a592074938118
Alexander Medina <alexjmedina@users.noreply.github.com>
2020-02-04 20:35:56 -0500

    Update EDA_basic.py

--- Preparation/EDA_basic.py
+++ Preparation/EDA_basic.py
@@ -1,3 +1,11 @@
+# Read train data
+# taxi_train = pd.read_csv('taxi_train.csv') # taxi_train rename as train
+train.columns.to_list()
+
+# Read test data
+# taxi_test = pd.read_csv('taxi_test.csv') # taxi_test rename as test
+test.columns.to_list()
+
 # Shapes of train and test data
 print('Train shape:', train.shape)
 print('Test shape:', test.shape)
